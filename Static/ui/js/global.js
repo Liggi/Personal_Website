e7bcdb67c6187d5e9052f56ad5b75d49da1c24b3
Jason Liggi <jason@liggi.net>
2012-09-16 17:26:25 +0100

    Finishing off contact panel functionality

--- Static/ui/js/global.js
+++ Static/ui/js/global.js
@@ -7,4 +7,18 @@ $(function () {
 			$(this).fadeOut();
 		});
 	});
+	
+	$('nav[role=navigation] ul li:last-of-type').click(function() {
+		$('.tab-overlay').show();
+		$('.contact-overlay').show();
+		
+		return false;
+	});
+	
+	$('.contact-overlay a:contains("Cancel")').click(function () {
+		$('.tab-overlay').hide();
+		$('.contact-overlay').hide();
+		
+		return false;		
+	});
 });
